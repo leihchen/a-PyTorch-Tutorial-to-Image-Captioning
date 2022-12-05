b8fbe8299993490082f9b011de9813d552f06de4
LeihaoChen <chenandrew683@gmail.com>
2022-12-05 03:07:03 -0500

    2

--- create_input_files.py
+++ create_input_files.py
@@ -3,7 +3,7 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
+                       karpathy_json_path='/Users/harddrive/Downloads/caption data/dataset_coco.json',
                        image_folder='/Users/harddrive/Downloads/coco_src',
                        captions_per_image=5,
                        min_word_freq=5,
